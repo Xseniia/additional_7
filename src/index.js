830a02b180a74454563d79ae0ef3fe393d252729
Xeniia <khitroxs@gmail.com>
2018-09-20 17:20:55 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  for(let a = 0; a < 9; a++) {
+  	for(let b = 0; b < 9; b++) {
+  		if(matrix[a][b] == 0) {
+  			for(let c = 1; c <= 9; c++) {
+  				if(checkXY(matrix, a, b, c) && checkCube(matrix, a, b, c)) {
+  					matrix[a][b] = c;
+  					if(solveSudoku(matrix)) {
+  						return matrix;
+  					}
+  					matrix[a][b] = 0;
+  				}
+  			}
+  			return false;
+  		}
+  	}
+  }
+  return true;
+}
+
+function checkXY(matrix, a, b, c) {
+	for(let x = 0; x < 9; x++) {
+		if(a != x && matrix[x][b] == c || b != x && matrix[a][x] == c) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function checkCube(matrix, a, b, c) {
+	for(let p = 0; p < 3; p++) {
+		for(let q = 0; q < 3; q++) {
+			if(a != p && b != q && matrix[Math.floor(a / 3) * 3 + p][Math.floor(b / 3) * 3 + q] == c) {
+				return false;
+			}
+		}
+	}
+	return true;
 }
